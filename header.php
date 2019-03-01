fef1ce080bb322583225cbfddf4da7705a99966e
Jared <jaybiedev@gmail.com>
2019-02-28 17:21:47 -0700

    fixing hamburger

--- header.php
+++ header.php
@@ -105,7 +105,7 @@ elseif ($online_giving_method == 'other') {
                     <div class="container-fluid">
                         <!-- Brand and toggle get grouped for better mobile display -->
                         <div class="navbar-header">
-                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse" aria-expanded="false">
                                 <span class="sr-only">Toggle navigation</span>
                                 <span class="fa fa-bars fa-2x"></span>
                             </button>
