85384948f3bcc913ba269641d0db1c817f2f5a41
Bret Miller <bret.miller@hq.gci.org>
2021-05-18 07:40:10 -0400

    v1.0.8 - Minor fix to Spol class to detect when xml is not retrieved.

--- lib/classes/Spol.php
+++ lib/classes/Spol.php
@@ -37,40 +37,43 @@ class Spol {
 			$url="https://www.gci.org/feed/?post_type=videos&media-categories=speaking-of-life&attach=video";
 	    		
 			$xml = simplexml_load_file($url);
+			
+			if ($xml) {
 
-			$namespaces = $xml->getNamespaces(true); // get namespaces
-			 
-			$items = array();
-			foreach ($xml->channel->item as $item) {
-			  $tmp = new stdClass();
-			  $tmp->id = trim((string) $item->children($namespaces['yt'])->videoId);
-			  $tmp->title = trim((string) $item->title);
-			  $tmp->author  = 'Grace Communion International';
-			  $tmp->uri  = $xml->channel->link;
-			  $tmp->updated =  date('Y-m-d', strtotime(trim((string) $item->pubDate)));
-			  $tmp->link = trim((string) $item->link);
-			  $tmp->url = trim((string) $item->enclosure->attributes()->url);
-			  $tmp->thumbnail = 'https://i1.ytimg.com/vi/'.$tmp->id.'/hqdefault.jpg';
-			  $tmp->description = trim((string) $item->description);
-			    
-			  
-              // wp post attribs
-              $tmp->post_title = $tmp->title;
-			  $tmp->thumbnail_url = $tmp->thumbnail;
-			  $tmp->permalink = $tmp->link;
-			  $tmp->post_type = 'media-youtube-spol';
-			  $tmp->post_date = $tmp->updated;
-			  $tmp->post_modified = $tmp->updated;
-			  
-			  $remove_url_regex = "@(https?://([-\w\.]+[-\w])+(:\d+)?(/([\w/_\.#-]*(\?\S+)?[^\.\s])?)?)@";			 
-			  $tmp->caption = preg_replace($remove_url_regex, "", $tmp->description);
-			  
-			  $items[] = $tmp;
+				$namespaces = $xml->getNamespaces(true); // get namespaces
+				 
+				$items = array();
+				foreach ($xml->channel->item as $item) {
+				  $tmp = new stdClass();
+				  $tmp->id = trim((string) $item->children($namespaces['yt'])->videoId);
+				  $tmp->title = trim((string) $item->title);
+				  $tmp->author  = 'Grace Communion International';
+				  $tmp->uri  = $xml->channel->link;
+				  $tmp->updated =  date('Y-m-d', strtotime(trim((string) $item->pubDate)));
+				  $tmp->link = trim((string) $item->link);
+				  $tmp->url = trim((string) $item->enclosure->attributes()->url);
+				  $tmp->thumbnail = 'https://i1.ytimg.com/vi/'.$tmp->id.'/hqdefault.jpg';
+				  $tmp->description = trim((string) $item->description);
+					
+				  
+				  // wp post attribs
+				  $tmp->post_title = $tmp->title;
+				  $tmp->thumbnail_url = $tmp->thumbnail;
+				  $tmp->permalink = $tmp->link;
+				  $tmp->post_type = 'media-youtube-spol';
+				  $tmp->post_date = $tmp->updated;
+				  $tmp->post_modified = $tmp->updated;
+				  
+				  $remove_url_regex = "@(https?://([-\w\.]+[-\w])+(:\d+)?(/([\w/_\.#-]*(\?\S+)?[^\.\s])?)?)@";			 
+				  $tmp->caption = preg_replace($remove_url_regex, "", $tmp->description);
+				  
+				  $items[] = $tmp;
+				}
+
+				$fp = fopen($this->file, 'w');
+				fwrite($fp, json_encode($items));
+				fclose($fp);
 			}
-
-			$fp = fopen($this->file, 'w');
-			fwrite($fp, json_encode($items));
-			fclose($fp);
 		}
 
 		$str = file_get_contents($this->file);
