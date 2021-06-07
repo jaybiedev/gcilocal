ce86281b407fa79cf6f6306f4d16ab0d95548b2a
Bret Miller <bret.miller@gci.org>
2021-06-07 10:41:10 -0400

    v1.0.9 - Fix SpOL Feed
    Check for SpOL daily
    Check http status code for SpOL feed
    Output SpOL feed errors in html comments.

--- lib/classes/Spol.php
+++ lib/classes/Spol.php
@@ -18,7 +18,7 @@ class Spol {
     	    $this->file .= "/spol.json";
     		if (file_exists($this->file)) {
     			$filetime = filemtime($this->file);
-    			$this->nexttime = strtotime("+7 day", $filetime);
+    			$this->nexttime = strtotime("+1 day", $filetime);
     		}
 	    }
 	}
@@ -35,44 +35,74 @@ class Spol {
 			//$url = "https://www.youtube.com/feeds/videos.xml?channel_id=" . $yt_channel_id;
 			// Office Speaking of Life feed
 			$url="https://www.gci.org/feed/?post_type=videos&media-categories=speaking-of-life&attach=video";
-	    		
-			$xml = simplexml_load_file($url);
+	    	libxml_use_internal_errors();	
+			// stopped working....  $xml = simplexml_load_file($url);
 			
-			if ($xml) {
+			$query_header = array();
+			$ch = curl_init();
+			curl_setopt($ch,CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+			curl_setopt($ch,CURLOPT_RETURNTRANSFER,1); 
+			curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,60);
+			curl_setopt($ch,CURLOPT_PORT, 443);
+			curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0); 
+			curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2); 
+			curl_setopt($ch,CURLOPT_URL,$url);
+			curl_setopt($ch,CURLOPT_HTTPHEADER, $query_header);
+			$content = curl_exec($ch);
+			$curlinf = curl_getinfo($ch);
+			curl_close($ch);
 
-				$namespaces = $xml->getNamespaces(true); // get namespaces
-				 
-				$items = array();
-				foreach ($xml->channel->item as $item) {
-				  $tmp = new stdClass();
-				  $tmp->id = trim((string) $item->children($namespaces['yt'])->videoId);
-				  $tmp->title = trim((string) $item->title);
-				  $tmp->author  = 'Grace Communion International';
-				  $tmp->uri  = $xml->channel->link;
-				  $tmp->updated =  date('Y-m-d', strtotime(trim((string) $item->pubDate)));
-				  $tmp->link = trim((string) $item->link);
-				  $tmp->url = trim((string) $item->enclosure->attributes()->url);
-				  $tmp->thumbnail = 'https://i1.ytimg.com/vi/'.$tmp->id.'/hqdefault.jpg';
-				  $tmp->description = trim((string) $item->description);
-					
-				  
-				  // wp post attribs
-				  $tmp->post_title = $tmp->title;
-				  $tmp->thumbnail_url = $tmp->thumbnail;
-				  $tmp->permalink = $tmp->link;
-				  $tmp->post_type = 'media-youtube-spol';
-				  $tmp->post_date = $tmp->updated;
-				  $tmp->post_modified = $tmp->updated;
-				  
-				  $remove_url_regex = "@(https?://([-\w\.]+[-\w])+(:\d+)?(/([\w/_\.#-]*(\?\S+)?[^\.\s])?)?)@";			 
-				  $tmp->caption = preg_replace($remove_url_regex, "", $tmp->description);
-				  
-				  $items[] = $tmp;
+			if ($curlinf['http_code'] != '200') {
+				echo "\n<!-- ------------------------------------------------------------------------------------------\n";
+				echo "Error fetching $url\n";
+				echo "HTTP response code ".$curlinf['http_code'].", content follows\n";
+				print_r($content);
+				echo "------------------------------------------------------------------------------------------- -->\n";
+			} else {
+				$xml=simplexml_load_string($content);
+				if ($xml) {
+
+					$namespaces = $xml->getNamespaces(true); // get namespaces
+					 
+					$items = array();
+					foreach ($xml->channel->item as $item) {
+					  $tmp = new stdClass();
+					  $tmp->id = trim((string) $item->children($namespaces['yt'])->videoId);
+					  $tmp->title = trim((string) $item->title);
+					  $tmp->author  = 'Grace Communion International';
+					  $tmp->uri  = $xml->channel->link;
+					  $tmp->updated =  date('Y-m-d', strtotime(trim((string) $item->pubDate)));
+					  $tmp->link = trim((string) $item->link);
+					  $tmp->url = trim((string) $item->enclosure->attributes()->url);
+					  $tmp->thumbnail = 'https://i1.ytimg.com/vi/'.$tmp->id.'/hqdefault.jpg';
+					  $tmp->description = trim((string) $item->description);
+						
+					  
+					  // wp post attribs
+					  $tmp->post_title = $tmp->title;
+					  $tmp->thumbnail_url = $tmp->thumbnail;
+					  $tmp->permalink = $tmp->link;
+					  $tmp->post_type = 'media-youtube-spol';
+					  $tmp->post_date = $tmp->updated;
+					  $tmp->post_modified = $tmp->updated;
+					  
+					  $remove_url_regex = "@(https?://([-\w\.]+[-\w])+(:\d+)?(/([\w/_\.#-]*(\?\S+)?[^\.\s])?)?)@";			 
+					  $tmp->caption = preg_replace($remove_url_regex, "", $tmp->description);
+					  
+					  $items[] = $tmp;
+					}
+
+					$fp = fopen($this->file, 'w');
+					fwrite($fp, json_encode($items));
+					fclose($fp);
+				} else {
+					echo "\n<!-- ------------------------------------------------------------------------------------------\n";
+					echo "Error parsing SpOL feed\n\n";
+					foreach (libxml_get_errors() as $xe) {
+						echo "Error ".cstr($xe->code)." at line ".cstr($xe->line)." column ".cstr($xe->column)."\n    ".$xe->message."\n\n";
+					}
+					echo "------------------------------------------------------------------------------------------- -->\n";
 				}
-
-				$fp = fopen($this->file, 'w');
-				fwrite($fp, json_encode($items));
-				fclose($fp);
 			}
 		}
 
